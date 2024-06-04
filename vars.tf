5d49761911be26b907bd9defe4feccb0bdfafd48
samsin16 <rajasinha082@gmail.com>
2024-06-04 13:02:37 +0530

    Add files via upload

---
+++ vars.tf
@@ -0,0 +1,5 @@
+variable compartment_ocid { default = "ocid1.compartment.oc1..aaaaaaaakuxrucuh4ucsx6chmvjwkvyqcd7af7kcxiaguaa6lr7z57vegs4q" }
+variable oci_core_instance--source_details-source_id--export_OCI-Instance-Terraform_1 { default = "ocid1.image.oc1.iad.aaaaaaaa3w4j6qpavgz53qiyw3hij76jxksgf2tjcrhlbp6yac77rfhtat2q" }
+variable availability_domain--qCah-US-ASHBURN-AD-1 { default = "qCah:US-ASHBURN-AD-1" }
+variable fault_domain--FAULT-DOMAIN-1 { default = "FAULT-DOMAIN-1" }
+variable region { default = "us-ashburn-1" }
